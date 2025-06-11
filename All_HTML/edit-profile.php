47c435d5b07afd180ec6e8b75da3c71a220c0bc8
MHZ48 <clashmalek111@gmail.com>
2025-06-11 15:17:16 +0300

    Revert "test deplo"
    This reverts commit a5bb14b15402185f6adab07df24a17956a625e0f.

--- All_HTML/edit-profile.php
+++ All_HTML/edit-profile.php
@@ -6,7 +6,7 @@ $username = isset($_SESSION['username']) ? $_SESSION['username'] : 'Guest';
 
 ?> 
 <?php
-require_once __DIR__ . '/../vendor/autoload.php'; // Adjust path as needed
+require_once 'C:/xampp/vendor/autoload.php'; 
 require_once '../connection.php'; // or your DB connection
 $imgUrl = isset($_SESSION['profile_img']) && !empty($_SESSION['profile_img'])
     ?  'https://www.w3schools.com/w3images/avatar2.png'
@@ -16,6 +16,7 @@ $imgUrl = isset($_SESSION['profile_img']) && !empty($_SESSION['profile_img'])
 
 
 use Cloudinary\Cloudinary;
+
 $cloudinary = new Cloudinary([
     'cloud' => [
         'cloud_name' => 'dn8gqkmpu',
@@ -419,4 +420,4 @@ input{
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
-</html>composer init
+</html>
